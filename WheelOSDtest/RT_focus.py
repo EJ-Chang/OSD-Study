6f6c71bb666568e0976be1b3efc13f8435ec3738
EJ-Chang <ola7984@gmail.com>
2020-04-06 17:18:27 +0800

    Using function to determine reponse key & feedback

--- WheelOSDtest/RT_focus.py
+++ WheelOSDtest/RT_focus.py
@@ -50,14 +50,30 @@ imageList = sorted(imageList) # Make a list of stimulus pics
 experiment_timer = core.Clock()
 experiment_timer.reset()
 MAX_DURATION = 5 # Unit: second
-expStatus = 1
+
 # nTrial = 1
 nStimulus = 1
-item = 1
+item = 0
 pre_Mouse = []
 # Get stimulus randomly sorted
-stimulus_seq = random.shuffle(imageList)
+random.shuffle(imageList)
+stimulus_seq = tuple(imageList)
+print(len(stimulus_seq))
 
+def response_key(userInput, userInput_time, stimuli):
+    global expStatus, item
+    if userInput[2] == 1:
+        print(userInput_time)
+        expStatus = 0
+        # return expStatus
+        # break
+    elif userInput[0] == 1:
+        stimuli = stimuli + 1
+        if stimuli > len(stimulus_seq) - 1:
+            stimuli = len(stimulus_seq) - 1
+        print('Next stimuli.')
+        item = stimuli
+expStatus = 1
 # Preparing experiment trials
 # for nTrial in range(12):
 while expStatus == 1:
@@ -68,12 +84,18 @@ while expStatus == 1:
 
     # Set break button
     buttons, times = mouse.getPressed(getTime = True)
-    if buttons[2] == 1:
-        print(times)
-        break
-    elif buttons[1] == 1 : # Need response interval = 250
-        item = item + 1
-        print(times)
+
+    response_key(buttons, times, item)
+
+
+    # if buttons[2] == 1:
+    #     print(times)
+    #     break
+    # elif buttons[0] == 1 : # Need response interval = 250
+    #     item = item + 1
+    #     if item > 11:
+    #         item = 11 # Python initiates from 0
+    #     print(times)
     # my_win.flip()
 
 # == == ==  EXp Ends == ==  == 
