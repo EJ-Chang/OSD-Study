6fb2168d45c4d3e98a9c58ad1b220fe6130115f7
EJ-Chang <ola7984@gmail.com>
2020-04-06 14:48:04 +0800

    Able to save a shuffled list.

--- WheelOSDtest/fileImport.py
+++ WheelOSDtest/fileImport.py
@@ -10,16 +10,14 @@ for file in os.listdir(path):
         imageList.append(os.path.join(path, file))
 
 imageList = sorted(imageList)
-presentList = []
+
 # print(imageList[2])
 seq = [1,2,3,4,5,6,7,8,9,10,11,12]
-for seq in range(3):
+presentList = []
+for n in range(3):
     random.shuffle(seq)
-    print(seq)
-    presentList.append(seq)
-    print(presentList)  # Q: how to save current sequnce?
-
-# for n in range(3):
-    # print(imageList[seq[n]])
-
-# print(random.randrange(4))
+    n_seq = tuple(seq)
+    presentList.append(n_seq)
+    print(n_seq)
+    # presentList = [presentList[index] for index in seq]
+    print(presentList[n])  
